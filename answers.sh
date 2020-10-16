cb589a565f188bbcd4be65b6fb3ad3bf8ffe89cd
farahhaddar <farah.haddar99@gmail.com>
2020-10-16 13:07:04 +0300

    BASIC QUERIES

---
+++ answers.sh
@@ -0,0 +1,8 @@
+BASIC QUERIES:
+1-SELECT name FROM students;
+2-SELECT * FROM students WHERE Age>30;
+3-SELECT * FROM students WHERE age =30 AND gender = "F";//none
+4-SELECT Name,Points FROM students WHERE Name="Alex";
+5-INSERT INTO students (Name, Age, Gender, Points) VALUES("Farah", 21, "F", 1000);
+6-UPDATE students SET points=(points+20) WHERE name="Basma";
+7-UPDATE students SET points=(points-10) WHERE name="Alex"; 
